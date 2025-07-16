2b71db2e6fb225abde6f904a59c1745bd7d20206
0xDEAD10CK <joshua.brennan.niu@gmail.com>
2025-07-16 12:09:47 +1200

    Created basic structure of application.

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return "Welcome to homepage!"
+
+@app.route('/about')
+def about():
+	return "Welcome to About!"
+
+if __name__ == '__main__':
+	app.run(debug=True)
