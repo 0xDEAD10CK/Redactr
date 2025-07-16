7a6f58fcdb15bfa4dec47ebd5e5d9e96dcb80f35
0xDEAD10CK <joshua.brennan.niu@gmail.com>
2025-07-16 14:18:41 +1200

    Added Applicant model to app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config.from_pyfile("config.py")
+db = SQLAlchemy(app)
+
+# --- Models ---
+class Applicant(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	full_name = db.Column(db.String(120))
+	email = db.Column(db.String(120))
+	phone = db.Column(db.string(20))
+	resume_filename = db.Column(db.String(200))
+	photo_filename = db.Column(db.String(200))
+	created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 @app.route('/')
 def home():
