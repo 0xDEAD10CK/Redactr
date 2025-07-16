4dc805e3c16c9c94e30d2a1be97d52ddb7fb1fca
0xDEAD10CK <joshua.brennan.niu@gmail.com>
2025-07-16 13:41:54 +1200

    Created static folder and added Application route to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,6 +9,10 @@ def home():
 @app.route('/about')
 def about():
 	return "Welcome to About!"
+
+@app.route('/apply')
+def apply():
+	return render_template("apply.html")
 
 if __name__ == '__main__':
 	app.run(debug=True)
